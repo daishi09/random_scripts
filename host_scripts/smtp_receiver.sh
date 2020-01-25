c5d6c450125498ff96a50b7eb613af5799fb1619
Alan Cantrell <alan.b.cantrell.ctr@mail.mil>
2020-01-24 19:45:23 -0500

    updated and commented smtp rcv and snd

--- host_scripts/smtp_receiver.sh
+++ host_scripts/smtp_receiver.sh
@@ -1,38 +1,60 @@
+#!/bin/bash
 #####################################################################################################
 # Script_Name : smtp_receiver.sh
 # Description : simulates smtp traffic with custom message
-# Date : November 2019
+# Date : January 2020
 ####################################################################################################
 
-# ----- Create a script to run smtp_sender.py while sending errors to /dev/null to avoid filling logs
-cat > /root/py_autorun.sh <<"__EOF__"
+# ----- Create a script to run smtp_receiver.py as a service while sending errors to /dev/null to avoid filling logs
+cat > /root/smtp_receiver.py << "__EOF__"
+import smtpd
+import asyncore
+
+class CustomSMTPServer(smtpd.SMTPServer):
+
+    def process_message(self, peer, mailfrom, rcpttos, data):
+        print 'Receiving message from:', peer
+        print 'Message addressed from:', mailfrom
+        print 'Message addressed to  :', rcpttos
+        print 'Message length        :', len(data)
+        return
+
+server = CustomSMTPServer(('0.0.0.0', 25), None)
+
+asyncore.loop()
+__EOF__
+
+# ----- Create a script to run smtp_receiver.py while sending errors to /dev/null to fix log filling issue
+cat > /root/py_rcv_autorun.sh <<"__EOF__"
 #!/bin/sh
 while [ true ]
 do
-  /usr/bin/python /root/smtp_receiver.py 2>/dev/null
+ /usr/bin/python /root/smtp_receiver.py 2>/dev/null
 done
 __EOF__
 
-chmod +x /root/py_autorun.sh
+# ----- Make autorun script executable
+chmod +x /root/py_rcv_autorun.sh
 
-# ----- Create service to autorun python script for smtp receiver
-cat > /lib/systemd/system/py_autorun.service <<"__EOF__"
+# ----- Create py_rcv_autorun service
+cat > /lib/systemd/system/py_rcv_autorun.service <<"__EOF__"
 [Unit]
-Description=Python Autorun Service
+Description=Python SMTP Receiver Autorun Service
 
 After=network.target
 StartLimitInterval=0
 
 [Service]
 Type=simple
-Restart=always
+Restart=on-failure
 RestartSec=1
 User=root
-ExecStart=/bin/bash /root/py_autorun.sh
+ExecStart=/bin/bash /root/py_rcv_autorun.sh
 
 [Install]
 WantedBy=multi-user.target
 __EOF__
 
-systemctl start py_autorun.service
-systemctl enable py_autorun.service
+# ----- Start and enable python receiver service
+systemctl start py_rcv_autorun.service
+systemctl enable py_rcv_autorun.service
--- host_scripts/smtp_sender.sh
+++ host_scripts/smtp_sender.sh
@@ -1,26 +1,51 @@
+#!/bin/bash
 #####################################################################################################
 # Script_Name : smtp_sender.sh
 # Description : simulates smtp traffic with custom message
-# Date : November 2019
+# Date : January 2020
 ####################################################################################################
 
+# ----- Create a script to run smtp_sender.py as a service while sending errors to /dev/null to avoid filling logs
+cat > /root/smtp_sender.py <<"__EOF__"
+import time
+import smtplib
+import email.utils
+from email.mime.text import MIMEText
 
-# ----- Create a script to run smtp_sender.py while sending errors to /dev/null to avoid filling logs
-cat > /root/py_autorun.sh <<"__EOF__"
+while True:
+
+  time.sleep(30)
+  # ---- email message
+  msg = MIMEText('Leroy, the username and password for the server at 172.16.101.2 is horde:forthehorde. You can begin uploading your findings there!')
+  msg['To'] = email.utils.formataddr(('Recipient', 'leeroyjenkins@gmail.com'))
+  msg['From'] = email.utils.formataddr(('Author', 'igotchicken@ymail.com'))
+  msg['Subject'] = 'creds4fun'
+
+  server = smtplib.SMTP('0.0.0.0', 25)
+  server.set_debuglevel(True) # show communication with the server
+
+  try:
+    server.sendmail('igotchicken@ymail.com', ['leeroyjenkins@gmail.com'], msg.as_string())
+  finally:
+    server.quit()
+__EOF__
+
+# ----- Create a script to run smtp_sender.py while sending errors to /dev/null to fix log filling issue
+cat > /root/py_snd_autorun.sh <<"__EOF__"
 #!/bin/sh
 while [ true ]
 do
-  /usr/bin/python /root/smtp_receiver.py 2>/dev/null
+  /usr/bin/python /root/smtp_sender.py 2>/dev/null
 done
 __EOF__
 
-chmod +x /root/py_autorun.sh
+# ----- Make autorun script executable
+chmod +x /root/py_snd_autorun.sh
 
-
-# ----- Create service to autorun python script for smtp sender
-cat > /lib/systemd/system/py_autorun.service <<"__EOF__"
+# ----- Create py_snd_autorun service
+cat > /lib/systemd/system/py_snd_autorun.service <<"__EOF__"
 [Unit]
-Description=Python Autorun Service
+Description=Python SMTP Sender Autorun Service
 
 After=network.target
 StartLimitInterval=0
@@ -30,11 +55,12 @@ Type=simple
 Restart=always
 RestartSec=1
 User=root
-ExecStart=/bin/bash /root/py_autorun.sh
+ExecStart=/bin/bash /root/py_snd_autorun.sh
 
 [Install]
 WantedBy=multi-user.target
 __EOF__
 
-systemctl start py_autorun.service
-systemctl enable py_autorun.service
+# ----- Start and enable python sender service
+systemctl start py_snd_autorun.service
+systemctl enable py_snd_autorun.service
